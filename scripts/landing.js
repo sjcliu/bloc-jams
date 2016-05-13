a8d340d2d1cc64cfb07ef6e47a2c8630717d0d1d
Stanly Liu <sjcliu@gmail.com>
2016-05-13 16:06:09 +0800

    completed assingment 22

--- scripts/landing.js
+++ scripts/landing.js
@@ -3,28 +3,14 @@
     // gets the elements with the class name point
     var points = document.getElementsByClassName('point');
     //changes the styles in the CSS file to the styles below.
-    var revealFirstPoint = function() {
-      points[0].style.opacity = 1;
-      points[0].style.transform = 'scaleX(1) translateY(0)';
-      points[0].style.msTransform = 'scaleX(1) translateY(0)';
-      points[0].style.WebkitTransform = 'scaleX(1) translateY(0)';
+    var revealPoint = function(index) {
+      points[index].style.opacity = 1;
+      points[index].style.transform = 'scaleX(1) translateY(0)';
+      points[index].style.msTransform = 'scaleX(1) translateY(0)';
+      points[index].style.WebkitTransform = 'scaleX(1) translateY(0)';
     };
-
-    var revealSecondPoint = function() {
-      points[1].style.opacity = 1;
-      points[1].style.transform = 'scaleX(1) translateY(0)';
-      points[1].style.msTransform = 'scaleX(1) translateY(0)';
-      points[1].style.WebkitTransform = 'scaleX(1) translateY(0)';
+    //loops through the elements with the class name point and applies the revealPoint CSS styles
+    for(var i=0; i < points.length; i++) {
+      revealPoint(i);
     };
-
-    var revealThirdPoint = function() {
-      points[2].style.opacity = 1;
-      points[2].style.transform = 'scaleX(1) translateY(0)';
-      points[2].style.msTransform = 'scaleX(1) translateY(0)';
-      points[2].style.WebkitTransform = 'scaleX(1) translateY(0)';
-    };
-
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
   };
