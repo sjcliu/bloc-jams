0d7a9693dcc3c36cb22dbaec1e3e98d13c9c3ad1
Stanly Liu <sjcliu@gmail.com>
2016-05-13 17:55:12 +0800

    done

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,8 @@
-// Update the styles set in the CSS file to the new styles in the script.
-  var animatePoints = function() {
-    // gets the elements with the class name point
-    var points = document.getElementsByClassName('point');
+  // gets the elements with the class name point
+  var points = document.getElementsByClassName('point');
+  // Update the styles set in the CSS file to the new styles in the script.
+  var animatePoints = function(points) {
+
     //changes the styles in the CSS file to the styles below.
     var revealPoint = function(index) {
       points[index].style.opacity = 1;
@@ -14,3 +15,18 @@
       revealPoint(i);
     };
   };
+  window.onload = function() {
+        // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+       if (window.innerHeight > 950) {
+           animatePoints(pointsArray);
+       }
+       
+       var sellingPoints = document.getElementsByClassName('selling-points')[0];
+       var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+       window.addEventListener("scroll", function(event) {
+           if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+               animatePoints(pointsArray);
+           }
+       });
+   }
