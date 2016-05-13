6c0b42341f418c838e51c8ab4784c712803c25bd
Stanly Liu <sjcliu@gmail.com>
2016-05-13 17:24:40 +0800

    assignment 23 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,30 @@
-// Update the styles set in the CSS file to the new styles in the script.
-  var animatePoints = function() {
-    // gets the elements with the class name point
-    var points = document.getElementsByClassName('point');
-    //changes the styles in the CSS file to the styles below.
-    var revealPoint = function(index) {
-      points[index].style.opacity = 1;
-      points[index].style.transform = 'scaleX(1) translateY(0)';
-      points[index].style.msTransform = 'scaleX(1) translateY(0)';
-      points[index].style.WebkitTransform = 'scaleX(1) translateY(0)';
-    };
+  //Selects the element with class name point
+  var pointsArray = document.getElementsByClassName('point');
+  //changes the styles in the CSS file to the styles below.
+  var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = 'scaleX(1) translateY(0)';
+    point.style.msTransform = 'scaleX(1) translateY(0)';
+    point.style.WebkitTransform = 'scaleX(1) translateY(0)';
+  };
+  //Animates the point element
+  var animatePoints = function(points){
     //loops through the elements with the class name point and applies the revealPoint CSS styles
-    for(var i=0; i < points.length; i++) {
-      revealPoint(i);
+    forEach(points, revealPoint);
+  };
+  // When the page loads and user scroll to the seliingPoints, animatePoints is executed
+  window.onload = function() {
+    //Automatically animate the points on a tall screen where scrolling can't trigger the animation.
+    if (window.innerHeight > 950) {
+      animatePoints(pointsArray);
     };
+
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener("scroll", function(event) {
+      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+        animatePoints(pointsArray);
+      }
+    });
   };
