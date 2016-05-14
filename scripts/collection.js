e3478315afd54edf8b644bfe687e2fce6e88a134
Stanly Liu <sjcliu@gmail.com>
2016-05-15 05:19:27 +0800

    fixed album link

--- scripts/collection.js
+++ scripts/collection.js
@@ -3,7 +3,7 @@ var collectionItemTemplate =
  + '  <img src="assets/images/album_covers/01.png"/>'
  + '  <div class="collection-album-info caption">'
  + '    <p>'
- + '      <a class="album-name" href="/album.html"> The Colors </a>'
+ + '      <a class="album-name" href="album.html"> The Colors </a>'
  + '      <br/>'
  + '      <a href="/album.html"> Pablo Picasso </a>'
  + '      <br/>'
@@ -19,7 +19,7 @@ var collectionItemTemplate =
    var collectionContainer = document.getElementsByClassName('album-covers')[0];
    //clears content for collectionContainer innerHTML property
    collectionContainer.innerHTML = '';
-   // inserts 12 albums 
+   // inserts 12 albums
    for (var i=0; i < 12; i++) {
      collectionContainer.innerHTML += collectionItemTemplate
    };
